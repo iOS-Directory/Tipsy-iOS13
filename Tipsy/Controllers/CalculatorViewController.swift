b13932c56d01679f07503ff7759acb9bbbfa9475
fritzgt <tuanis@live.com>
2019-11-29 11:43:49 +0200

    completed logic

--- Tipsy/Controllers/CalculatorViewController.swift
+++ Tipsy/Controllers/CalculatorViewController.swift
@@ -17,20 +17,56 @@ class CalculatorViewController: UIViewController {
     @IBOutlet weak var twentyPctButton: UIButton!
     @IBOutlet weak var splitNumberLabel: UILabel!
     
-    
-    
+ 
+    var pctNum: Double?
+    var splitNum: Int?
     //IBActions for all inputs
+    
+    //IBAction for the tip buttons
     @IBAction func tipChanged(_ sender: UIButton) {
-        print(sender.currentTitle!)
+        //On button pressed clear all active buttons
+        clearTipButtons()
+        
+        //Set the pressed button to active
+        sender.isSelected = true
+        
+        //Get the value of the button
+        let pctStr =  sender.currentTitle!
+        
+        //Turn the value into a decimal
+        pctNum = (pctStr as NSString).doubleValue / 100
+        billTextField.endEditing(true)
     }
     
     
+    //IBAction for the split button -+
     @IBAction func stepperValueChanged(_ sender: UIStepper) {
-        print(sender.value)
+        //get value and turn it into an Int
+        splitNum = Int(sender.value)
+        
+        //Set the label = to the value
+        splitNumberLabel.text = String(splitNum!)
     }
     
+    //IBAction for calculate button
     @IBAction func calculatePressed(_ sender: UIButton) {
-        print("Pressed")
+        
+        //Get the ammount
+        let totalBill = billTextField.text
+        
+        //Turn string into a Double for the equation
+        let totalNum = (totalBill! as NSString).doubleValue
+    billTextField.endEditing(true)
+        
+        //Calculate precentage and split then turn it in a Float
+        let totalForEach = Float(((pctNum! * totalNum) + totalNum) / Double(splitNum!))
+    }
+    
+    //Clear all the tip buttons before activating   one
+    func clearTipButtons(){
+        zeroPctButton.isSelected = false
+        tenPctButton.isSelected = false
+        twentyPctButton.isSelected = false
     }
     
 }
